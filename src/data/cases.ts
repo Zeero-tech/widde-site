0226acff0d99796f374ab56bc26e93fc5777f7a7
Igor Moura <igor@widde.io>
2026-04-13 13:57:03 -0300

    imagens

--- src/data/cases.ts
+++ src/data/cases.ts
@@ -3,7 +3,7 @@ import type { CasePost } from '@/types'
 export const cases: CasePost[] = [
   {
     bg: '',
-    image: '../../public/cases/mamo-case.jpg',
+    image: '/cases/mamo-case.jpg',
     category: 'Construindo narrativas com Video Commerce',
     title: 'Mamô',
     desc: '“O que mais me surpreendeu foi a quantidade de visualização. Eu não esperava que as clientes fossem gostar tanto assim dos vídeos, as visualizações foram muito boas.”',
@@ -11,7 +11,7 @@ export const cases: CasePost[] = [
   },
   {
     bg: '',
-    image: '../../public/cases/jhonjhon-case.jpg',
+    image: '/cases/jhonjhon-case.jpg',
     category: 'Agregando valor ao produto com o Video Commerce',
     title: 'John John',
     desc: '"Com a Widde nós recebemos os comentários dos clientes e usuários do site. A partir deles a gente foi formatando o nosso tipo de conteúdo que trazia mais engajamento. Tem clientes que responderam “Gosto de vídeos que mostrem mais detalhes das peças ou de looks” e a gente foi adaptando até chegar nesse formato que é o ideal."',
@@ -19,7 +19,7 @@ export const cases: CasePost[] = [
   },
   {
     bg: '',
-    image: '../../public/cases/majeste-case.jpg',
+    image: '/cases/majeste-case.jpg',
     category: 'Desafios da Moda Festa no E-commerce',
     title: 'Majesté',
     desc: '"Começamos usando vídeos que a gente já tinha para as redes sociais, fizemos alguns ajustes e começamos a usar na Widde. Passou o tempo, a gente foi se adaptando e conhecendo a solução. Então, começamos a fazer vídeos específicos para a Widde. Antigamente, era vídeo que a gente fazia para redes sociais e usava na Widde, e agora os vídeos que fazemos para a Widde usamos na rede social também."',
