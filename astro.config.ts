8ef317998d264bd93ee509cf1c5f97b29455bd8c
pedrovaferreira <51165638+pedrovaferreira@users.noreply.github.com>
2026-06-09 21:43:45 -0300

    fix: sitemap xml

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  site: 'https://widde.io',
   output: 'static',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
